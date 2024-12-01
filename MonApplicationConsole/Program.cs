8a1c200e4f6ba1f5fe07d0ed6624c64154e7e8dd
Zahira <zahiramachraoui@gmail.com>
2024-12-01 15:01:45 +0100

    Ajoutez des fichiers projet.

---
+++ MonApplicationConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MonApplicationConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Affiche un message d'accueil
+            Console.WriteLine("Bienvenue dans mon application console !");
+
+            // Demande le nom de l'utilisateur
+            Console.Write("Entrez votre nom : ");
+            string nom = Console.ReadLine();
+
+            // Demande l'âge de l'utilisateur
+            Console.Write("Entrez votre âge : ");
+            int age = int.Parse(Console.ReadLine());
+
+            // Affiche un message personnalisé
+            Console.WriteLine($"Bonjour {nom}, vous avez {age} ans.");
+
+            // Attendre que l'utilisateur appuie sur une touche pour fermer
+            Console.WriteLine("Appuyez sur une touche pour quitter...");
+            Console.ReadKey();
+        }
+    }
+}
